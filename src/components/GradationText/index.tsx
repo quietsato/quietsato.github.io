0288e74ab10f5bf5983588c34217c3c7cf4f5498
quietsato <quietsato@gmail.com>
2022-05-28 17:36:10 +0900

    [feat] Add GradationText component

---
+++ src/components/GradationText/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+const Span = styled.span`
+  display: inline-block;
+  background: linear-gradient(
+    180deg,
+    var(--theme-yellow) 0%,
+    var(--theme-blue) 20%,
+    var(--theme-green) 100%
+  );
+  background: -webkit-linear-gradient(
+    -90deg,
+    var(--theme-yellow) 0%,
+    var(--theme-blue) 20%,
+    var(--theme-green) 100%
+  );
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+const GradationText: React.FC<{}> = (props) => {
+  return <Span>{props.children}</Span>;
+};
+
+export default GradationText;
