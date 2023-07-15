49f2033e2b36baefc2c0b96f3191277f58a84271
Taiki Sato <quietsato@gmail.com>
2023-07-15 21:37:44 +0900

    feat: add animation to gradient text

--- src/components/GradationText/index.tsx
+++ src/components/GradationText/index.tsx
@@ -2,23 +2,30 @@ import React from "react";
 import styled from "styled-components";
 
 const Span = styled.span`
+  @keyframes animated-gradient-text {
+    0% {
+      background-position: 0% 50%;
+    }
+    100% {
+      background-position: 100% 50%;
+    }
+  }
+
   display: inline-block;
   background: linear-gradient(
-    180deg,
-    var(--theme-yellow) 0%,
-    var(--theme-blue) 20%,
-    var(--theme-green) 100%
-  );
-  background: -webkit-linear-gradient(
-    -90deg,
-    var(--theme-yellow) 0%,
-    var(--theme-blue) 20%,
-    var(--theme-green) 100%
+    to right,
+    var(--theme-yellow) 20%,
+    var(--theme-red) 40%,
+    var(--theme-cyan) 60%,
+    var(--theme-yellow) 80%
   );
   background-clip: text;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
+  background-size: 500% auto;
+  animation: animated-gradient-text 5s linear infinite;
 `;
+
 
 export default function GradationText({ children } : { children: React.ReactNode }) {
   return <Span>{children}</Span>;
